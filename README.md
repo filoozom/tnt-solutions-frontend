8b53c9d8c70a8338f52775130832587998cf54e7
Philippe Schommers <philippe@schommers.be>
2018-07-11 19:04:51 +0200

    Add link to GitHub repo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Frontend for tierion.services
 
-Based on [preact](https://github.com/developit/preact) and [preact-cli](https://github.com/developit/preact-cli).
+Based on [preact](https://github.com/developit/preact) and [preact-cli](https://github.com/developit/preact-cli), used in addition with [tnt-solutions-backend](https://github.com/filoozom/tnt-solutions-backend)
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,7 @@ import { h, Component } from 'preact'
 import Toolbar from 'preact-material-components/Toolbar'
 
 import 'preact-material-components/Toolbar/style.css'
+import style from './style'
 
 export default class Header extends Component {
   render() {
@@ -11,6 +12,11 @@ export default class Header extends Component {
           <Toolbar.Row>
             <Toolbar.Section align-start>
               <Toolbar.Title>Tierion calculator</Toolbar.Title>
+            </Toolbar.Section>
+            <Toolbar.Section align-end className={style['toolbar-right']}>
+              <a href="https://github.com/filoozom/tnt-solutions-frontend">
+                <img src="/assets/icons/github.png" />
+              </a>
             </Toolbar.Section>
           </Toolbar.Row>
         </Toolbar>
